ea5f4e2829066370cb343c21b3c3bb8a78da7851
Rishabh <roy.ranjan.rrm@gmail.com>
2025-03-13 14:37:53 +0530

    transition

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/hero";
-import Projects from "./components/Project";
+import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Blog from "./components/Blog";
